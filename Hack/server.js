fd71f9c53d9944b075554be235f004e6d4efab3a
syedalihax <Syedalihax@gmail.com>
2026-08-13 17:30:22 +0500

    import userModel in server.js

--- Hack/server.js
+++ Hack/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
 const connectDB = require("./config/db")
+const UserModel = require("./models/User")
 
 dotenv.config()
 connectDB()
