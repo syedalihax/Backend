acb1f15c571f37b555775549e1cc845762351b72
syedalihax <Syedalihax@gmail.com>
2026-08-13 16:43:47 +0500

    basic server.js complete

---
+++ Hack/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+app.use(express.json())
+
+let PORT = process.env.PORT
+
+if (PORT == undefined) {
+    PORT = 3000
+    console.log("PORT is not defined in .env file so by default PORT is set to 3000")
+}
+
+app.listen(3000, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
