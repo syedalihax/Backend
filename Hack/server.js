aff662f7f34747d6b2ca22a5f5c663af25b0c952
syedalihax <Syedalihax@gmail.com>
2026-08-13 16:52:20 +0500

    mongodb config import in server.js

--- Hack/server.js
+++ Hack/server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
+const connectDB = require("./config/db")
 
 dotenv.config()
+connectDB()
 
 app.use(express.json())
 
