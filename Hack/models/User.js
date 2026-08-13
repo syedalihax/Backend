08f160839cdfde0fbe96f458bd1855243ad3a204
syedalihax <Syedalihax@gmail.com>
2026-08-13 17:27:21 +0500

    User Schema and Model created

---
+++ Hack/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name:{type:String , required:[true , "name is required"] },
+    email:{type:String , required:[true , "email is requried"] , unique:[true , "this email is registered"]},
+    password:{type:String , required:[true , "password is requried"] },
+    role:{type:String , default:"user" , enum:['user' , 'admin']}
+})
+const UserModel = mongoose.model("userModel" , userSchema)
+
+module.exports = UserModel
