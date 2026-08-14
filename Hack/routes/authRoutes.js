01a5b46cc7234e800dfb25b86d2e75e2a9b0a099
syedalihax <Syedalihax@gmail.com>
2026-08-14 15:29:10 +0500

    register api complete

--- Hack/routes/authRoutes.js
+++ Hack/routes/authRoutes.js
@@ -1,1 +1,7 @@
-const 
+const express = require("express")
+const router = express.Router()
+const {register} = require("../controllers/authController")
+
+router.post("/register" , register)
+
+module.exports = router
