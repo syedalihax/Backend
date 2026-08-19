e5ebb0e7edf04ad935e96ba10ae26f3e777fc484
syedalihax <Syedalihax@gmail.com>
2026-08-19 09:43:01 +0500

    jwt verificationing middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,55 @@
+const jwt = require("jsonwebtoken")
+
+const middleware = (req, res, next) => {
+    const header = req.headers.Authorization
+
+    if (!header) {
+        return (
+            res.status(401).json({
+                success: false,
+                message: "Unauthorized"
+            })
+        )
+    }
+    console.log(header)
+
+    const token = header.split(" ")[1]
+
+    if (!token) {
+        return (
+            res.status(401).json({
+                success: false,
+                message: "Unauthorized"
+            })
+        )
+    }
+
+    console.log(token)
+
+    try {
+        const verifiedToken = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (!verifiedToken) {
+            return (
+                res.status(401).json({
+                    success: false,
+                    message: "Unauthorized"
+                })
+            )
+        }
+        console.log(verifiedToken)
+        req.userId = verifiedToken.userId
+        next()
+
+    }
+
+    catch (error) {
+
+        return (
+            res.status(401).json({
+                success: false,
+                message: error.message
+            })
+        )
+    }
+}
