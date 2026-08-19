37639c5a5e6cb3fb18bc853540862f3481d8e358
syedalihax <Syedalihax@gmail.com>
2026-08-19 09:09:52 +0500

    change the user schema to don't sent the password to response

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
     name:{type:String , required:[true , "name is required"] },
     email:{type:String , required:[true , "email is requried"] , unique:[true , "this email is registered"]},
-    password:{type:String , required:[true , "password is requried"] },
+    password:{type:String , required:[true , "password is requried"] , select : false },
     role:{type:String , default:"user" , enum:['user' , 'admin']}
 })
 const UserModel = mongoose.model("user" , userSchema)
