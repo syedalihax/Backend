da03732fca5b89c1688657de43bb96bc9d872f31
syedalihax <Syedalihax@gmail.com>
2026-08-19 09:43:41 +0500

    jwt stored in local storage for learing stage only

--- frontend_old/app.js
+++ frontend_old/app.js
@@ -205,7 +205,8 @@ if (loginForm) {
 
                 console.log("Login successful:", data);
 
-
+                localStorage.setItem("token", data.token);
+                
                 showPopup(
                     "Login Successful!",
                     "Welcome back. You are now logged in.",
