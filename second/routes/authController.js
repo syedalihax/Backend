2a01df430c3dc6382115e9a84e517b9844db7087
syedalihax <syedalihax@gmail.com>
2026-08-26 10:03:49 -0700

    file name changed

--- second/routes/authController.js
+++
@@ -1,8 +0,0 @@
-const {register , login , logOut} = require("../controllers/authController")
-const express = require("express")
-const router = express.Router()
-
-router.post("/register" , register)
-router.post("/login" , login)
-router.post("/logout" , logOut)
-module.exports = router
