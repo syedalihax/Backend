9971747afb3fd326a790b6fa91c4d773f8d7c89e
syedalihax <syedalihax@gmail.com>
2026-08-25 18:56:44 -0700

    new Practice : Register api and login api complete

--- second/server.js
+++ second/server.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const connectDB = require("./config/db.js")
-
+const authRoutes = require("./routes/authController")
 dotenv.config()
 
 const app = express()
+app.use(express.json())
 
 let port = process.env.port
 
@@ -13,6 +14,8 @@ if(!port){
 }
 
 connectDB()
+
+app.use("/api/auth" , authRoutes)
 
 app.listen(port , ()=>{
     console.log(` -- server is running on port ${port} -- `)
