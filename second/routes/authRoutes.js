5b610236096b758ceaef7d5593ced9f4082b5ddc
syedalihax <Syedalihax@gmail.com>
2026-08-27 12:52:20 +0500

    fix the path

---
+++ second/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const {register , login , logOut} = require("../controllers/authController")
+const express = require("express")
+const router = express.Router()
+
+router.post("/register" , register)
+router.post("/login" , login)
+router.post("/logout" , logOut)
+module.exports = router
