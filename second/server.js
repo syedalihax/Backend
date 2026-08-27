bfb7fae3e288fe8da2d53cf2f658dcb242b8521c
syedalihax <syedalihax@gmail.com>
2026-08-27 11:19:40 -0700

    update : changes from new features

--- second/server.js
+++ second/server.js
@@ -4,10 +4,6 @@ const connectDB = require("./config/db.js")
 const authRoutes = require("./routes/authRoutes")
 const userRoutes = require("./routes/userRoutes")
 const productRoutes = require("./routes/productRoutes")
-<<<<<<< HEAD
-
-=======
->>>>>>> d02238169f99fd8665863cdfc642b78e133a2d81
 dotenv.config()
 
 const app = express()
@@ -23,11 +19,8 @@ connectDB()
 
 app.use("/api/auth" , authRoutes)
 app.use("/api/users", userRoutes);
-<<<<<<< HEAD
+
 app.use("/api/products" , productRoutes)
-=======
-app.use("/api" , productRoutes)
->>>>>>> d02238169f99fd8665863cdfc642b78e133a2d81
 
 app.listen(port , ()=>{
     console.log(` -- server is running on port ${port} -- `)
