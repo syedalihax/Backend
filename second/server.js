42030de4bc0678ed267503a9b3e85421fee74539
syedalihax <Syedalihax@gmail.com>
2026-08-28 22:20:34 +0500

    done

--- second/server.js
+++ second/server.js
@@ -23,12 +23,7 @@ connectDB()
 
 app.use("/api/auth" , authRoutes)
 app.use("/api/users", userRoutes);
-<<<<<<< HEAD
 app.use("/api/products" , productRoutes)
-=======
-app.use("/api" , productRoutes)
->>>>>>> d02238169f99fd8665863cdfc642b78e133a2d81
-
 app.listen(port , ()=>{
     console.log(` -- server is running on port ${port} -- `)
 })
